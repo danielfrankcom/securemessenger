42c8e96499d5634cd12b93e74c358523dcf31ce6
Daniel Frankcom <danielfrankcom@gmail.com>
2017-10-30 17:58:04 -0700

    Added some sweeet documentation

--- Client.java
+++ Client.java
@@ -1,19 +1,35 @@
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/*
+* Client portion of the client/server communication
+* Initialize communication with the server
+* Send messages to server and receive messages from server
+*/
 class Client implements Runnable{
     private Thread t;
     private Server sv;
     public BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
-   
+    
+    /*
+    * Initialize the thread
+    */
     Client() {
         System.out.println("Client initializing");
     }
 
+    /*
+    * Locally assign the server thread
+    * @return      void
+    */
     public void assign(Server server){
         sv = server;
     }
    
+    /*
+    * Run the thread code
+    * @return      void
+    */
     public void run(){
         System.out.println("Client running");
         try{
@@ -25,6 +41,10 @@ class Client implements Runnable{
         System.out.println("Client exiting");
     }
     
+    /*
+    * Create a thread to run
+    * @return      void
+    */
     public void start (){
         System.out.println("Client starting");
         if (t == null) {
--- Server.java
+++ Server.java
@@ -1,19 +1,35 @@
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/*
+* Server portion of the client/server communication
+* Wait for initialization from client
+* Receive messages from client and send messages to client
+*/
 class Server implements Runnable {
    private Thread t;
    private Client cl;
    public BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
    
+    /*
+    * Initialize the thread
+    */
     Server() {
         System.out.println("Server initializing");
     }
     
+    /*
+    * Locally assign the client thread
+    * @return      void
+    */
     public void assign(Client client){
         cl = client;
     }
    
+    /*
+    * Run the thread code
+    * @return      void
+    */
     public void run(){
         System.out.println("Server running");
         try{
@@ -24,7 +40,11 @@ class Server implements Runnable {
         }
         System.out.println("Server exiting");
     }
-   
+
+    /*
+    * Create a thread to run
+    * @return      void
+    */
     public void start (){
         System.out.println("Server starting");
         if (t == null) {
