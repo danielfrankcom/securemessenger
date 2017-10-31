2c8f6b6a1c90965d96b8395849129ab86e981c4a
Daniel Frankcom <danielfrankcom@gmail.com>
2017-10-30 19:44:44 -0700

    Commented and formatted Client.java
    Still need to make variable names more clear, and remove unnecessary protocol steps

--- Client.java
+++ Client.java
@@ -16,10 +16,11 @@ import javax.crypto.interfaces.*;
 class Client implements Runnable{
     private Thread t;
     private Server sv;
-    public BlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>();
-    public byte[] encodedParams; //need to share the spec
-    public int aliceLen;
     private Cipher aliceCipher;
+
+    public BlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>(); //store messages from server
+    public byte[] encodedParams; //need to be the same between communicating parties
+    public int aliceLen; //this is not useful, but needs to be communicated via network at one point
 
     /*
     * Initialize the thread
@@ -43,15 +44,10 @@ class Client implements Runnable{
     public void run(){
         System.out.println("Client running");
 
-        try{
-
-            //System.out.println(queue.take()); //this waits until there is a message to take          
-
-            generateKey(exchangeSecrets());
-     
-            byte[] recovered = aliceCipher.doFinal(queue.take());
-            System.out.println("Encrypted: " + new String(recovered));
-
+        try{         
+            generateKey(getSharedSecret());
+            String message = new String(aliceCipher.doFinal(queue.take())); //get from queue and decrypt message
+            System.out.println("Encrypted: " + message);
         }catch(Exception e){
             System.out.println("Exception caught: " + e.getCause().getMessage());
         }
@@ -71,54 +67,60 @@ class Client implements Runnable{
         }
     }
 
-    private byte[] exchangeSecrets(){
-        byte[] aliceSharedSecret = null;
+    /*
+    * Get a shared secret between client and server
+    * @return      byte[] shared secret
+    */
+    private byte[] getSharedSecret(){
         try{
 
             KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH");
             aliceKpairGen.initialize(2048);
             KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
-            // Alice creates and initializes her DH KeyAgreement object
-            System.out.println("ALICE: Initialization ...");
             KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
             aliceKeyAgree.init(aliceKpair.getPrivate());
             
-            // Alice encodes her public key, and sends it over to Bob.
             byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded();
-            sv.queue.put(alicePubKeyEnc); //put into server message queue
+            sv.queue.put(alicePubKeyEnc); //put public key into server message queue
             System.out.println("client sent key");
 
-            byte[] bobPubKeyEnc = queue.take();
+            byte[] bobPubKeyEnc = queue.take(); //wait for server to generate a public key
 
             KeyFactory aliceKeyFac = KeyFactory.getInstance("DH");
             X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
             x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
             PublicKey bobPubKey = aliceKeyFac.generatePublic(x509KeySpec);
-            System.out.println("ALICE: Execute PHASE1 ...");
             aliceKeyAgree.doPhase(bobPubKey, true);
 
-            aliceSharedSecret = aliceKeyAgree.generateSecret();
-            aliceLen = aliceSharedSecret.length;
-            sv.queue.put("done".getBytes());
-            queue.take();
+            byte[] aliceSharedSecret = aliceKeyAgree.generateSecret(); //store shared secret
+            aliceLen = aliceSharedSecret.length; //set public var for use by server (or later send over network)
+            sv.queue.put("done".getBytes()); //notify server that this var has been set (or later not necessary)
+            queue.take(); //wait for server to process
+            return aliceSharedSecret;
+
         }catch(Exception e){
-            System.out.println("Exception caught.");
+            System.out.println("Exception caught: " + e.getCause().getMessage());
         }
-        return aliceSharedSecret;
+
+        return null;
     }
 
+    /*
+    * Use shared secret to make a shared key
+    * @return      void
+    */
     private void generateKey(byte[] aliceSharedSecret){
-
         try{
-            SecretKeySpec aliceAesKey = new SecretKeySpec(aliceSharedSecret, 0, 16, "AES");
+
+            SecretKeySpec aliceAesKey = new SecretKeySpec(aliceSharedSecret, 0, 16, "AES"); //use shared secret
             AlgorithmParameters aesParams = AlgorithmParameters.getInstance("AES");
             aesParams.init(encodedParams);
             aliceCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            aliceCipher.init(Cipher.DECRYPT_MODE, aliceAesKey, aesParams);
+            aliceCipher.init(Cipher.DECRYPT_MODE, aliceAesKey, aesParams); //initialize private variable to store cipher
+
         }catch(Exception e){
             System.out.println("Exception caught.");
         }
-
     }
 
 }
